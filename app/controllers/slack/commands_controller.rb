77edf00462886ad5291117a7f45bcfc6350e4f66
Asad Ullah <62687519+asad-u@users.noreply.github.com>
2021-03-15 13:54:11 +0500

    find or create user when called with command (#13)

--- app/controllers/slack/commands_controller.rb
+++ app/controllers/slack/commands_controller.rb
@@ -2,7 +2,7 @@ module Slack
   class CommandsController < ApplicationController
     include SlackCommands
     skip_before_action :verify_authenticity_token
-    before_action :verify_slack_request, :set_organization
+    before_action :verify_slack_request, :set_user, :set_organization
 
     def birthday
       method = params[:text].blank? ? 'update_modal' : 'birthdays_listing'
@@ -13,7 +13,12 @@ module Slack
     private
 
     def set_organization
-      @organization = Organization.includes(:birthdays).find_by_slack_id params[:team_id]
+      @organization = Organization.includes(birthdays: :user).find_by_slack_id params[:team_id]
+    end
+
+    def set_user
+      @user = User.find_or_create_by slack_id: params[:user_id]
+      @user.update(full_name: params[:user_name]) if @user.full_name.blank?
     end
   end
 end

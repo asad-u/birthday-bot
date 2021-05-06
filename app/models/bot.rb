6ca123c33435cb20c6993b843029343c42f6dadd
Asad Ullah <asad.ullah@gsquad.io>
2021-05-06 21:14:41 +0500

    Update status to installed

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -11,7 +11,7 @@ class Bot < ApplicationRecord
       access_token: bot_info['access_token'], bot_id: bot_info['bot_user_id'],
       channel_name: bot_info['incoming_webhook']['channel'], channel_id: bot_info['incoming_webhook']['channel_id'],
       webhook_configuration_url: bot_info['incoming_webhook']['configuration_url'],
-      webhook_url: bot_info['incoming_webhook']['url']
+      webhook_url: bot_info['incoming_webhook']['url'], status: 'installed'
     }
     update_or_create({ source: 'slack' }, update_params)
   }
